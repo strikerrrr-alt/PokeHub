71e4191a7f04d0b0a595c926842423c69cfe7b28
Striker <zzstrikerzzyt@gmail.com>
2019-08-28 22:33:57 +0100

    Added Pidgey (016)

---
+++ assets/info/016_pidgey.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 13,
+    'species': '**Pidgey** (**ポッポ** *Poppo*)',
+    'types' : 'Normal | Flying',
+    'pokedexEntry': 'Pidgey has an extremely sharp sense of direction. It is capable of unerringly returning home to its nest, however far it may be removed from its familiar surroundings.\n\n*Dex entry from ORAS*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 255, percentage: '43.9%' },
+    'baseStats': { hp: 40, atk: 45, def: 40, spAtk: 35, spDef: 35, spd: 56 },
+    'abilities': { 0: 'Keen Eye or Tangled Feet', 'H': 'Big Pecks' },
+    'height': 0.3,
+    'weight': 4.0,
+    'colorHex': '#E4B86D',
+    'colorHexShiny': '#B5A85B',
+    'color': 'Soft Orange',
+    'colorShiny': 'Slightly Desaturated Yellow',
+    'evolvesTo': { species: 'Pidgeotto', level: '18', triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Flying'],
+    'hatchTime': '3855 - 4111 steps',
+    'gif': 'http://play.pokemonshowdown.com/sprites/xyani/pidgey.gif',
+    'gifShiny': 'http://play.pokemonshowdown.com/sprites/xyani-shiny/pidgey.gif',
+    'legendary': false,
+    'mythical': false
+};
