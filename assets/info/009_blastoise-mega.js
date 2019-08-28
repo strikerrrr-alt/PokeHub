0171e033fbbbd023f146a49d63904e3a65fd0450
Striker <zzstrikerzzyt@gmail.com>
2019-08-28 20:33:53 +0100

    Add Blastoise and Mega (009)

---
+++ assets/info/009_blastoise-mega.js
@@ -0,0 +1,21 @@
+exports.info = {
+    'number': 9,
+    'species': '**Mega Blastoise** (**カメックス** Kamekkusu)',
+    'types' : 'Water',
+    'pokedexEntry': 'The cannon on its back is as powerful as a tank gun. Its tough legs and back enable it to withstand the recoil from firing the cannon.\n\n*Dex entry from LGP/LGE*',
+    'genderRatio': { M: 0.875, F: 0.125 },
+    'catchRate': { integer: 45, percentage: '11.9%' },
+    'baseStats': { hp: 79, atk: 103, def: 120, spAtk: 135, spDef: 115, spd: 78 },
+    'abilities': { 0: 'Torrent', 'M': 'Mega Launcher', 'H': 'Rain Dash' },
+    'height': 1.6,
+    'weight': 101.1,
+    'colorHex': '#3088F0',
+    'color': 'Blue',
+    'evolvesFrom': { species: 'Blastoise', level: null, triggeredBy: 'Mega Evolution with Blastoisinite' },
+    'eggGroups': ['Monster', 'Water 1'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'http://play.pokemonshowdown.com/sprites/xyani/blastoise-mega.gif',
+    'gifShiny': 'http://play.pokemonshowdown.com/sprites/xyani-shiny/blastoise-mega.gif',
+    'legendary': false,
+    'mythical': false
+};
