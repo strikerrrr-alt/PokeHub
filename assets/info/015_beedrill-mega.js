e85f98d8be7a5446880ec5bad010434fb30d7526
Striker <zzstrikerzzyt@gmail.com>
2019-08-28 22:22:46 +0100

    Add Beedrill and Mega (015)

---
+++ assets/info/015_beedrill-mega.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 15,
+    'species': '**Mega Beedrill** (**スピアー** *Supiaa*)',
+    'types' : 'Bug | Poison',
+    'pokedexEntry': 'Its legs have become poison stingers. It stabs its prey repeatedly with the stingers on its limbs, dealing the final blow with the stinger on its rear.n\n*Dex entry from LGP/LGE*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 45, percentage: '11.9%' },
+    'baseStats': { hp: 65, atk: 150, def: 40, spAtk: 15, spDef: 80, spd: 145 },
+    'abilities': { 0: 'Swarm', 'M': 'Adaptability', 'H': 'Sniper' },
+    'height': 1.4,
+    'weight': 40.5,
+    'colorHex': '#FEE75C',
+    'colorHexShiny': '#8DD43A',
+    'color': 'Soft Yellow',
+    'colorShiny': 'Moderate Green',
+    'evolvesFrom': { species: 'Mega Beedrill', level: null, triggeredBy: 'Mega Evolution with Beedrillite' },
+    'eggGroups': ['Bug'],
+    'hatchTime': '3855 - 4111 steps',
+    'gif': 'http://play.pokemonshowdown.com/sprites/xyani/beedrill-mega.gif',
+    'gifShiny': 'http://play.pokemonshowdown.com/sprites/xyani-shiny/beedrill-mega.gif',
+    'legendary': false,
+    'mythical': false
+};
