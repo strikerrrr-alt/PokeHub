dd4fe8349f25ca3b80fa8b5e4c0df77014f46e87
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-28 22:24:56 -0700

    Added Kommo-o (784)
    Yay! Hope the 2 are right

---
+++ assets/info/784_kommo-o.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 784,
+    'species': '**Kommo-o** (**ジャラランガ** **jyararanga**)',
+    'types' : 'Dragon | Fighting',
+    'pokedexEntry': 'When it howls after finishing off its prey, the metallic sounds of its celebrating comrades can be heard from all around. It battles valiantly with its fists, which are armored in steel scales. Kommo-o specializes in uppercuts.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 45, percentage: '11.9%' },
+    'baseStats': { hp: 75, atk: 110, def: 125, spAtk: 100, spDef: 105, spd: 85 },
+    'abilities': { 0: 'Bulletproof *or* Soundproof', 'H': 'Overcoat' },
+    'height': 1.6,
+    'weight': 172.4,
+    'colorHex': '#C2BEB8',
+    'colorHexShiny': '#1ED63',
+    'color': 'Grayish Orange',
+    'colorShiny': 'Soft Yellow',
+    'evolvesFrom': { species: 'Hakamo-o', level: '45', triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Dragon'],
+    'hatchTime': '10280 - 10536 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/kommoo.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/kommoo.gif',
+    'legendary': false,
+    'mythical': false
+};
