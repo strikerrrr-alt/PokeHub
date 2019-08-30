bb390b5519e584db76e9f83d6546e976be615aac
Striker <34070194+Strikerrr@users.noreply.github.com>
2019-08-30 14:02:04 +0100

    Fixed Ditto

--- assets/info/132_ditto.js
+++ assets/info/132_ditto.js
@@ -3,7 +3,7 @@ exports.info = {
     'species': '**Ditto** (**メタモン** **Metamon**)',
     'types' : 'Normal',
     'pokedexEntry': 'While it can transform into anything, each Ditto apparently has its own strengths and weaknesses when it comes to transformations. It transforms into whatever it sees. If the thing it\'s transforming into isn\'t right in front of it, Ditto relies on its memory—so sometimes it fails.\n\n*Dex entry from USUM*',
-    'genderRatio': { Genderless },
+    'genderRatio': { m: 0, f: 0 },
     'catchRate': { integer: 35, percentage: '9.9%' },
     'baseStats': { hp: 48, atk: 48, def: 48, spAtk: 48, spDef: 48, spd: 48 },
     'abilities': { 0: 'Limber', 'H': 'Imposter' },
