604cc5ca80dcbc3da0e48b68d483b84985c13ed1
Striker <34070194+Strikerrr@users.noreply.github.com>
2019-08-30 13:45:32 +0100

    Update 286_Breloom.js

--- assets/info/286_Breloom.js
+++ assets/info/286_Breloom.js
@@ -1,7 +1,7 @@
     'number': 286,
     'species': '**Breloom** (**キノガッサ** **Kinogassa**)',
     'types' : 'Grass | Fighting',
-    'pokedexEntry': 'Breloom closes in on its foe with light and sprightly footwork, then throws punches with its stretchy arms. This Pokémon's fighting technique puts boxers to shame. The seeds ringing Breloom's tail are made of hardened toxic spores. It is horrible to eat the seeds. Just taking a bite of this Pokémon's seed will cause your stomach to rumble.\n\n*Dex entry from ORAS*',
+    'pokedexEntry': 'Breloom closes in on its foe with light and sprightly footwork, then throws punches with its stretchy arms. This Pokémon\'s fighting technique puts boxers to shame. The seeds ringing Breloom\'s tail are made of hardened toxic spores. It is horrible to eat the seeds. Just taking a bite of this Pokémon\'s seed will cause your stomach to rumble.\n\n*Dex entry from ORAS*',
     'genderRatio': { M: 0.5, F: 0.5 },
     'catchRate': { integer: 90, percentage: '20.1%' },
     'baseStats': { hp: 60, atk: 130, def: 80, spAtk: 60, spDef: 60, spd: 70 },
