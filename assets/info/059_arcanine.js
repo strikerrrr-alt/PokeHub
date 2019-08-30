36b9ff9cbd6007f7febd5b5c31e26929dc89bd73
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-29 20:40:32 -0700

    Added Arcanine (059)
    Woof

---
+++ assets/info/059_arcanine.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 059,
+    'species': '**Arcanine** (**ウインディ** **Windie**)',
+    'types' : 'Fire',
+    'pokedexEntry': 'Legends tell of its fighting alongside a general and conquering a whole country. There are so many old tales about them that they're called legendary Pokémon, but there are way more of them around than you'd expect.\n\n*Dex entry from GAME*',
+    'genderRatio': { M: 0.75, F: 0.25 },
+    'catchRate': { integer: 75, percentage: '17.5%' },
+    'baseStats': { hp: 90, atk: 110, def: 80, spAtk: 100, spDef: 80, spd: 95 },
+    'abilities': { 0: 'Intimidate *or* Flash Fire', 'H': 'Justified' },
+    'height': 1.9,
+    'weight': 341.7,
+    'colorHex': '#F09D5E',
+    'colorHexShiny': '#F5E42F',
+    'color': 'Soft Orange',
+    'colorShiny': 'Bright Yellow',
+    'evolvesFrom': { species: 'Growlithe', level: 'Null', triggeredBy: 'Evolution With Fire Stone' },
+    'eggGroups': ['Field'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/arcanine.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/arcanine.gif',
+    'legendary': false,
+    'mythical': false
+};
