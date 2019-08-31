5a36195ae799bcbf082b15043b8878058f4c13ef
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-31 16:19:56 -0700

    Added Fearow (022)

---
+++ assets/info/022_fearow.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 022,
+    'species': '**Fearow** (**オニドリル** **Onidrill**)',
+    'types' : 'Normal | Flying',
+    'pokedexEntry': 'Carrying food through Fearow\'s territory is dangerous. It will snatch the food away from you in a flash! In Alola, fish Pokémon are its prey. It can be seen circling above the ocean searching for food.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 90, percentage: '20.1%' },
+    'baseStats': { hp: 65, atk: 90, def: 65, spAtk: 61, spDef: 61, spd: 100 },
+    'abilities': { 0: 'Keen Eye', 'H': 'Sniper' },
+    'height': 1.2,
+    'weight': 83.8,
+    'colorHex': '#D59551',
+    'colorHexShiny': '#B0B165',
+    'color': 'Moderate Orange',
+    'colorShiny': 'Mostly Desaurated Dark Yellow',
+    'evolvesFrom': { species: 'Spearow', level: '20', triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Flying'],
+    'hatchTime': '3855 - 4111 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/fearow.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/fearow.gif',
+    'legendary': false,
+    'mythical': false
+};
