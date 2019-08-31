69c525873cb1f145e99dac1f753ec424e1f9e514
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-30 23:32:07 -0700

    Added Qwilfish (211)
    Spiky fish no one likes lol

---
+++ assets/info/211_qwilfish.js
@@ -0,0 +1,22 @@
+exports.info = {
+    'number': 211,
+    'species': '**Qwilfish** (**ハリーセン** **Harysen**)',
+    'types' : 'Water | Poison',
+    'pokedexEntry': 'Qwilfish sucks in water, inflating itself. This Pokémon uses the pressure of the water it swallowed to shoot toxic quills all at once from all over its body. It finds swimming somewhat challenging.\n\n*Dex entry from ORAS*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 45, percentage: '11.9%' },
+    'baseStats': { hp: 65, atk: 95, def: 75, spAtk: 55, spDef: 55, spd: 85 },
+    'abilities': { 0: 'Poison Point *or* Swift Swim', 'H': 'Intimidate' },
+    'height': 0.5,
+    'weight': 8.6,
+    'colorHex': '#477E7D',
+    'colorHexShiny': '#C170B4',
+    'color': 'Mostly Desaturated Dark Cyan',
+    'colorShiny': 'Slightly Desaturated Magenta',
+    'eggGroups': ['Water 2'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/qwilfish.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/qwilfish.gif',
+    'legendary': false,
+    'mythical': false
+};
