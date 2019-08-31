d7d8960492a7489c22421d5b72deead0b3a17021
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-30 23:55:22 -0700

    Added Drifloon (425)
    Child stealing balloon

---
+++ assets/info/425_drifloon.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 425,
+    'species': '**Drifloon** (**フワンテ** **Fuwante**)',
+    'types' : 'Ghost | Flying',
+    'pokedexEntry': 'Its round body is stuffed with souls and expands each time it leads someone away. Wandering souls gathered together to form this Pokémon. When trying to make friends with children, Drifloon grabs them by the hand.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 125, percentage: '25.7%' },
+    'baseStats': { hp: 90, atk: 50, def: 34, spAtk: 60, spDef: 44, spd: 70 },
+    'abilities': { 0: 'Aftermath *or* Unburden', 'H': 'Flare Boost' },
+    'height': 0.4,
+    'weight': 2.6,
+    'colorHex': '#A988B6',
+    'colorHexShiny': '#F9EC76',
+    'color': 'Slightly Desaturated Violet',
+    'colorShiny': 'Soft Yellow',
+    'evolvesTo': { species: 'Drifblim', level: '28', triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Amorphous'],
+    'hatchTime': '7710 - 7966 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/drifloon.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/drifloon.gif',
+    'legendary': false,
+    'mythical': false
+};
