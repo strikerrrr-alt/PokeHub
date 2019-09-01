2428057fd017afe08aecd6c3b428dab729401b50
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-31 20:22:03 -0700

    Added Mega Pidgeot (018)
    even BIGGER bird

---
+++ assets/info/018_pidgeot-mega.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 018,
+    'species': '**Mega Pidgeot** (**ピジョット** **Pigeot**)',
+    'types' : 'Normal | Flying',
+    'pokedexEntry': 'With its muscular strength now greatly increased, it can fly continuously for two weeks without resting.\n\n*Dex entry from LGP/LGE*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 45, percentage: '11.9%' },
+    'baseStats': { hp: 83, atk: 80, def: 80, spAtk: 135, spDef: 80, spd: 0 },
+    'abilities': { 0: 'Keen Eye *or* Tangled Feet', 'H': 'Big Pecks', 'M': 'No Guard' },
+    'height': 2.2,
+    'weight': 111.3,
+    'colorHex': '#FCA674',
+    'colorHexShiny': '#C6923E',
+    'color': 'Soft Orange',
+    'colorShiny': 'Beige',
+    'evolvesFrom': { species: 'Pidgeot', level: 'Null', triggeredBy: 'Mega Evolution with Pideotite' },
+    'eggGroups': ['Flying'],
+    'hatchTime': '3855 - 4111 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/pidgeot-mega.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/pidgeot-mega.gif',
+    'legendary': false,
+    'mythical': false
+};
