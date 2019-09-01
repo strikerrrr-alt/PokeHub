f906886e44c9f434e76edccb3cd20d5768aead74
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-08-31 21:42:21 -0700

    Added Arbok (024)
    HISSSSSSSSSS

---
+++ assets/info/024_arbok.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 024,
+    'species': '**Arbok** (**アーボック** **Arbok**)',
+    'types' : 'Poison',
+    'pokedexEntry': 'The latest research has determined that there are over 20 possible arrangements of the patterns on its stomach. After stunning its opponents with the pattern on its stomach, it quickly wraps them up in its body and waits for them to stop moving.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.5, F: 0.5 },
+    'catchRate': { integer: 90, percentage: '20.1%' },
+    'baseStats': { hp: 60, atk: 95, def: 69, spAtk: 65, spDef: 79, spd: 80 },
+    'abilities': { 0: 'Intimidate *or* Shed Skin', 'H': 'Unnerve' },
+    'height': 3.5,
+    'weight': 143.3,
+    'colorHex': '#B3A8C8',
+    'colorHexShiny': '#C6AA64',
+    'color': 'Grayish Violet',
+    'colorShiny': 'Slightly Desaturated Orange',
+    'evolvesFrom': { species: 'Ekans', level: '22', triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Field', 'Dragon'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/arbok.gif',
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/arbok.gif',
+    'legendary': false,
+    'mythical': false
+};
