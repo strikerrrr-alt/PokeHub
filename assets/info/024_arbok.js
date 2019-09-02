ab51ed1d61d45aa92f5172cdc771a3e27fd58c13
Striker <34070194+Strikerrr@users.noreply.github.com>
2019-09-02 11:19:09 +0100

    Update 024_arbok.js

--- assets/info/024_arbok.js
+++ assets/info/024_arbok.js
@@ -1,5 +1,5 @@
 exports.info = {
-    'number': 024,
+    'number': 24,
     'species': '**Arbok** (**アーボック** **Arbok**)',
     'types' : 'Poison',
     'pokedexEntry': 'The latest research has determined that there are over 20 possible arrangements of the patterns on its stomach. After stunning its opponents with the pattern on its stomach, it quickly wraps them up in its body and waits for them to stop moving.\n\n*Dex entry from USUM*',
