7dc8cacc256007becc39bbdd184f59bac508e7b3
Striker <34070194+Strikerrr@users.noreply.github.com>
2019-09-02 11:21:01 +0100

    Update 023_ekans.js

--- assets/info/023_ekans.js
+++ assets/info/023_ekans.js
@@ -1,5 +1,5 @@
 exports.info = {
-    'number': 023,
+    'number': 23,
     'species': '**Ekans** (**アーボ** **Arbo**)',
     'types' : 'Poison',
     'pokedexEntry': 'By dislocating its jaw, it can swallow prey larger than itself. After a meal, it curls up and rests. The eggs of bird Pokémon are its favorite food. It swallows eggs whole, so sometimes an egg gets stuck, and Ekans faints.\n\n*Dex entry from USUM*',
